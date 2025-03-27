ee47e6e6fd627ffd5e79641cbb23001318c9866c
Lipsdata <j1051505734@163.com>
2025-03-27 16:15:28 +0800

    Update hls.h

--- source/hls.h
+++ source/hls.h
@@ -1,5 +1,7 @@
 #ifndef HLS_CNN_H
 #define HLS_CNN_H
+
+#include "common.h" // 确保common.h在前面
 #if __VITIS_HLS__
     #include <ap_int.h>
 #endif
@@ -7,11 +9,9 @@
 #include <stdint.h>
 #include <stddef.h>
 #include "hls_config.h"
-#include "xmem.h"
 #include "common_with_hls.h"
 #include <math.h>
 #include <string.h>
-#include "common.h"
 
 #ifndef MAX_MAP_SIZE
 #define MAX_MAP_SIZE 1024  // 最大特征图大小 (32x32)
@@ -42,12 +42,23 @@ void HLS_DECLARE(conv_fprop2)(HLS_COMMON_ARG Layer *s2_pooling_layer, Layer *c3_
 void HLS_DECLARE(max_pooling_fprop2)(HLS_COMMON_ARG Layer *c3_conv_layer, Layer *s4_pooling_layer);
 void HLS_DECLARE(conv_fprop3)(HLS_COMMON_ARG Layer *s4_pooling_layer, Layer *c5_conv_layer, bool pconnection[96]);
 void HLS_DECLARE(fully_connected_fprop)(HLS_COMMON_ARG Layer *c5_conv_layer, Layer *output_layer); */
-void HLS_DECLARE(conv_fprop1)(HLS_COMMON_ARG Layer1 *input_layer1, Layer2 *input_layer2, Layer1 *c1_conv_layer1, Layer2 *c1_conv_layer2, uint8_t pconnection[96]);
-void HLS_DECLARE(max_pooling_fprop1)(HLS_COMMON_ARG Layer1 *c1_conv_layer1, Layer2 *c1_conv_layer2, Layer1 *s2_pooling_layer1, Layer2 *s2_pooling_layer2);
-void HLS_DECLARE(conv_fprop2)(HLS_COMMON_ARG Layer1 *s2_pooling_layer1, Layer2 *s2_pooling_layer2, Layer1 *c3_conv_layer1, Layer2 *c3_conv_layer2, uint8_t pconnection[96]);
-void HLS_DECLARE(max_pooling_fprop2)(HLS_COMMON_ARG Layer1 *c3_conv_layer1, Layer2 *c3_conv_layer2, Layer1 *s4_pooling_layer1, Layer2 *s4_pooling_layer2);
-void HLS_DECLARE(conv_fprop3)(HLS_COMMON_ARG Layer1 *s4_pooling_layer1, Layer2 *s4_pooling_layer2, Layer1 *c5_conv_layer1, Layer2 *c5_conv_layer2, uint8_t pconnection[96]);
-void HLS_DECLARE(fully_connected_fprop)(HLS_COMMON_ARG Layer1 *c5_conv_layer1, Layer2 *c5_conv_layer2, Layer1 *output_layer1, Layer2 *output_layer2);
+void HLS_DECLARE(conv_fprop1)(HLS_COMMON_ARG NetworkParams *input_params, InputLayer *input_layer, 
+                             NetworkParams *c1_params, C1Layer *c1_layer, uint8_t pconnection[96]);
+                             
+void HLS_DECLARE(max_pooling_fprop1)(HLS_COMMON_ARG NetworkParams *c1_params, C1Layer *c1_layer,
+                                    NetworkParams *s2_params, S2Layer *s2_layer);
+                                    
+void HLS_DECLARE(conv_fprop2)(HLS_COMMON_ARG NetworkParams *s2_params, S2Layer *s2_layer,
+                             NetworkParams *c3_params, C3Layer *c3_layer, uint8_t pconnection[96]);
+                             
+void HLS_DECLARE(max_pooling_fprop2)(HLS_COMMON_ARG NetworkParams *c3_params, C3Layer *c3_layer,
+                                    NetworkParams *s4_params, S4Layer *s4_layer);
+                                    
+void HLS_DECLARE(conv_fprop3)(HLS_COMMON_ARG NetworkParams *s4_params, S4Layer *s4_layer,
+                             NetworkParams *c5_params, C5Layer *c5_layer, uint8_t pconnection[96]);
+                             
+void HLS_DECLARE(fully_connected_fprop)(HLS_COMMON_ARG NetworkParams *c5_params, C5Layer *c5_layer,
+                                       NetworkParams *output_params, OutputLayer *output_layer);
 
 //void HLS_DECLARE(max_pooling_fprop)(HLS_COMMON_ARG Layer *prev_layer, Layer *layer);
 
